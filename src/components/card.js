e36b216baa581b222b0a5836fcc48a7d1351d71e
Sam Oberly <soberly1@jh.edu>
2022-11-03 02:10:35 +0000

    practice work

--- src/components/card.js
+++ src/components/card.js
@@ -2,7 +2,20 @@ import React from "react";
 import star from "../images/star.png";
 import zaferes from "../images/zeferes.png";
 
-export default function Card() {
+export default function Card(props) {
+  const firstName = "Sam";
+  const lastName = "Oberly";
+  const date = new Date();
+  const hours = date.getHours();
+  let timeOfDay;
+
+  if (hours < 12) {
+    timeOfDay = "morning";
+  } else if (hours >= 12 && hours < 17) {
+    timeOfDay = "afternoon";
+  } else {
+    timeOfDay = "night";
+  }
   return (
     <div className="card">
       <div className="card--image">
@@ -14,10 +27,12 @@ export default function Card() {
         <p className="card--dark-text">5.0</p>
         <p className="card--light-text">(6) &middot; USA</p>
       </div>
-      <p className="card--desc">Life lessons with Katie Zaferes</p>
+      <p className="card--desc">
+        Life lessons with {firstName + " " + lastName}
+      </p>
       <div className="card--price">
         <h5>From $136</h5>
-        <p>/ person</p>
+        <p>/ person starting at {timeOfDay}</p>
       </div>
     </div>
   );
